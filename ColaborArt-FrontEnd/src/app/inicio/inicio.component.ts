7c2bcfd9cfe77d7b6d030599e7160e2e00bfb6ac
laise <lg.souza15@gmail.com>
2021-01-31 15:33:39 -0300

    pequena alteração-palavras

--- ColaborArt-FrontEnd/src/app/inicio/inicio.component.ts
+++ ColaborArt-FrontEnd/src/app/inicio/inicio.component.ts
@@ -107,7 +107,7 @@ this.produto.usuario = this.user
 
 this.produtoService.postProduto(this.produto).subscribe((resp : Produto)=>{
   this.produto = resp
-  alert('Produto cadastrado realizada com sucesso!!')
+  alert('Produto cadastrado realizado com sucesso!!')
   this.produto = new Produto()
   this.getAllProdutos()
     })
